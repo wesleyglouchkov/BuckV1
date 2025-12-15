b8367ed91c836bdd8e8e106e692e128791f3d27d
Amcelo13 <tchetan308@gmail.com>
2025-12-15 04:49:53 -0500

    feat: Add explicit JWT token retrieval and Authorization header for client-side requests, removing custom cookie configuration.

--- actions/index.ts
+++ actions/index.ts
@@ -8,3 +8,11 @@ export const getUserRole = async () => {
     const session = await auth();
     return session?.user?.role || 'member';
 }
+
+
+export const getCookieTokenAction = async () => {
+    const cookieStore = await cookies()
+    const cookiesOfNextAuth = cookieStore.get(process.env.JWT_SALT as string)
+    // eslint-disable-next-line @typescript-eslint/no-non-null-asserted-optional-chain
+    return cookiesOfNextAuth?.value!
+}
--- utils/axios.ts
+++ utils/axios.ts
@@ -1,3 +1,4 @@
+import { getCookieTokenAction } from "@/actions";
 import axios from "axios";
 
 // Axios instance without authentication - for public endpoints
@@ -11,12 +12,14 @@ export const axiosInstance = axios.create({
 
 // Client-side authenticated axios instance (uses session)
 export const createClientAuthInstance = async ( role?: string) => {
+    const token =  await getCookieTokenAction()
     return axios.create({
         baseURL: `${process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:8000'}/api`,
         withCredentials: true, // Always include cookies for authentication
         headers: {
             ...(role && { 'role': role }),
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`,
         },
     });
 };
