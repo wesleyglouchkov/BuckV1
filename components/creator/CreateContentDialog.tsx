40281d351b3f076a5bd3887ca9b0659a7b4ce7d9
Amcelo13 <tchetan308@gmail.com>
2025-12-14 20:54:23 -0500

    refactor: remove unused `Link` import

--- components/creator/CreateContentDialog.tsx
+++ components/creator/CreateContentDialog.tsx
@@ -1,7 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 import { Plus, Video, Calendar } from "lucide-react";
-import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 
